eba8414c2fd93dabcb3b277e59150c51a4c5481e
alaakh <alaakhaled22111995@gmail.com>
2025-08-16 22:16:17 +0300

    add accountController and viewModels

---
+++ Presentation Layer/Areas/Identitiy/ViewModels/EmailConfirmationVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PresentationLayer.Areas.IDentitiy.ViewModels
+{
+    public class EmailConfirmationVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(256)]
+        public string UserNameOrEmail { get; set; } = null!;
+
+    }
+}
